2ab6a54638566079dcf428fcea1d0f22c1962683
Raphael Braga <raphael.lourenco.braga@gmail.com>
2018-06-26 23:24:09 -0300

    Banco de dados H2 e criação automatica da base de dados

--- src/main/java/com/optimusoft/cursomc/models/Categoria.java
+++ src/main/java/com/optimusoft/cursomc/models/Categoria.java
@@ -2,7 +2,12 @@ package com.optimusoft.cursomc.models;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Categoria implements Serializable {
 
 	/**
@@ -10,8 +15,8 @@ public class Categoria implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
 	private String nome;
