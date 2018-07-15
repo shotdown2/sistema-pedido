0de6f086ffcda84ee93eed16e05b7e0e991ce194
Raphael Braga <raphael.lourenco.braga@gmail.com>
2018-07-14 22:31:13 -0300

    Validacao sintatica com Bean validation

--- src/main/java/com/optimusoft/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/optimusoft/cursomc/dto/CategoriaDTO.java
@@ -2,6 +2,10 @@ package com.optimusoft.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.optimusoft.cursomc.models.Categoria;
 
 public class CategoriaDTO implements Serializable {
@@ -12,6 +16,9 @@ public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
+	
+	@NotEmpty(message="Campo nome obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 
 	public CategoriaDTO() {
--- src/main/java/com/optimusoft/cursomc/services/CategoriaService.java
+++ src/main/java/com/optimusoft/cursomc/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.optimusoft.cursomc.dto.CategoriaDTO;
 import com.optimusoft.cursomc.models.Categoria;
 import com.optimusoft.cursomc.repositories.CategoriaRepository;
 import com.optimusoft.cursomc.services.exceptions.DataIntegrityException;
@@ -62,5 +63,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repository.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
