3835c2f80022f9313e8bbce545fb5cff908b6cbb
Raphael Braga <raphael.lourenco.braga@gmail.com>
2018-08-17 16:50:29 -0300

    Ajustando e testando a deleção de Cliente

--- src/main/java/com/optimusoft/cursomc/models/Cliente.java
+++ src/main/java/com/optimusoft/cursomc/models/Cliente.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -36,7 +37,7 @@ public class Cliente implements Serializable {
 	@Enumerated(EnumType.STRING)
 	private TipoCliente tipo;
 
-	@OneToMany(mappedBy = "cliente")
+	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
 
 	@JsonIgnore
