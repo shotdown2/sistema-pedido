6b3d959bf802384652c90dfa83b3ea3b9851b50a
Raphael Braga <raphael.lourenco.braga@gmail.com>
2018-09-01 17:23:03 -0300

    Acesso para permissão heroku corsConfigurer

--- src/main/java/com/optimusoft/cursomc/CursomcApplication.java
+++ src/main/java/com/optimusoft/cursomc/CursomcApplication.java
@@ -3,6 +3,10 @@ package com.optimusoft.cursomc;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
@@ -14,5 +18,15 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
 	}
 }
