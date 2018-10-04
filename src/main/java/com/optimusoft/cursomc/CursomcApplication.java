2f0c85ca9885a5b85ac986e516525ce4737ba66c
Raphael Braga <raphael.lourenco.braga@gmail.com>
2018-10-04 18:24:58 -0300

    Salvando arquivo no S3

--- src/main/java/com/optimusoft/cursomc/CursomcApplication.java
+++ src/main/java/com/optimusoft/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.optimusoft.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,8 +9,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import com.optimusoft.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -17,7 +23,7 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		s3Service.uploadFile("//Users//raphaellourencobraga//Documents//Raphael//Trabalho//Ionic//sistema-pedido//src//assets//imgs//tela-inicial.png");
 	}
 	
 	@Bean
